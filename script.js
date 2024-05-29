9051ab824c469ec57825fd79df1b902985fb6ec8
System Administrator <root@MacBook-Pro-de-User.local>
2024-05-29 10:21:12 -0600

    Adicion de validacion de existencia de cliente

--- script.js
+++ script.js
@@ -32,12 +32,12 @@ loginData.addEventListener('click', event =>{
         headers: {
             'Content-Type': 'application/json'
         },
-        body: JSON.stringify(formData)
+        body: JSON.stringify(loginRequest)
     }).then(res => res.json())
       .then(data => console.log(data))
 })
 
-// popup
+// popup rfid
 let openPopUp = document.getElementById('openPopup');
 let Popup = document.getElementById('popup');
 let buttonIniciarPIN = document.getElementById('iniciarPIN');
@@ -49,17 +49,52 @@ openPopUp.addEventListener('click', (event)=>{
     Popup.style.visibility = "visible"; 
 });
    
-
-
 // cerrar popup
-// Popup.onclick = function(){
-//     Popup.style.visibility = "hidden";
-// }
-
-buttonIniciarPIN.addEventListener('click', (event)=>{
-    event.preventDefault();
-})
-
 close.onclick = function(){
     Popup.style.visibility = "hidden";
 }
+
+//BOTON INICIAR POPUP
+buttonIniciarPIN.addEventListener('click', (event)=>{
+    event.preventDefault();
+});
+
+//REQUEST PARA BOTON SIGNUP
+$(document).ready(function() {
+    $('#SignUp').click(function(event) {
+        event.preventDefault();
+        
+        let username = $('input[name="usernameReg"]').val();
+        let password = $('input[name="passwordReg"]').val();
+        let email = $('input[name="emailReg"]').val();
+        let id = $('input[name="idReg"]').val();
+        
+        let signUpRequest = {
+            user_name: username,
+            password: password,
+            email: email,
+            identification: id
+        }
+
+        console.log(signUpRequest);
+        fetch('https://atmsystemapibank.onrender.com/user/add_user', {
+        method:'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(signUpRequest)
+    }).then(res => res.json())
+      .then(res => {
+        if(res.data.requestCustomerData){   //SI NO SE TIENE UN CLIENTE REGISTRADO CON ESE DPI
+            let PopupAddCustomer = document.getElementById('PopupAddCustomer');
+            PopupAddCustomer.style.visibility = "visible"; 
+        }
+      })
+    });
+
+    $('#closeButtonAddCustomer').click( event => {
+        event.preventDefault();
+        let PopupAddCustomer = document.getElementById('PopupAddCustomer');
+        PopupAddCustomer.style.visibility = "hidden"; 
+    });
+});
